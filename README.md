dd9a181e8810141b89488cefb22cb294e8b7a663
saldfjj <semalex11@gmail.com>
2024-06-09 20:12:13 +0300

    Added realization of 'BrainEven Game' and 'Asciinema'

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SaldFjjHalsDj/fullstack-javascript-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SaldFjjHalsDj/fullstack-javascript-project-44/actions)
+### CodeClimate Badge:
 <a href="https://codeclimate.com/github/SaldFjjHalsDj/fullstack-javascript-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/327158e37bf7783da03c/maintainability" /></a>
+### Asciinema:
+https://asciinema.org/a/5jkfIFRogFuDzPaHQUzQhQxXQ
---
+++ bin/brain-even.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import helloUser from '../modules/userGreetings.js';
+import isEven from '../modules/isEven.js';
+
+const brainEven = () => {
+  const name = helloUser();
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let count = 0;
+  const answerList = ['no', 'yes'];
+
+  while (count <= 2) {
+    const number = Math.floor(Math.random() * 100);
+
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    const userAnswer = answerList.indexOf(answer);
+
+    if (userAnswer + isEven(number) === 2 || userAnswer + isEven(number) === 0) {
+      count += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answerList[isEven(number)]}'`);
+      count = 0;
+      break;
+    }
+  }
+
+  if (count > 2) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+brainEven();
+
+export default brainEven;
